a5a0dadaf5608208d2ecb9e1d85c0ac49c622264
ktipps <62031664+ktipps@users.noreply.github.com>
2020-09-19 09:59:18 -0400

    Trying to troubleshoot JavaScript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log("Your index.js file is loaded correctly");
 
 $(".btn").on("click", function(){
-    $(#talk-soon-confirmation).css("display, block");
+    $("#talk-soon-confirmation").css("display, block");
 });
