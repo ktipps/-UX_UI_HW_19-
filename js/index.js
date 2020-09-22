7fa8c8028edfcbe39c88a426d03d672063208475
ktipps <62031664+ktipps@users.noreply.github.com>
2020-09-22 10:06:32 -0400

    Updating JavaScript files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 console.log("Your index.js file is loaded correctly");
 
 $(".btn").on("click", function(){
-    $("#talk-soon-confirmation").css("display, block");
+    $("#talk-soon-confirmation").addClass("active");
+});
+
+$(".text-change").on("click", function(){
+    $("#talk-soon-confirmation").css("display", "block");
 });
